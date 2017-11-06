e425988e71097bbdf745f72e0c078fe1b68337ec
Ankit <ab9.bhatia@gmail.com>
2017-11-06 12:50:58 +0530

    q10

--- SB_SQL.sql
+++ SB_SQL.sql
@@ -19,27 +19,9 @@ select name,population from city where name in ('Phoenix','Los Angeles','Chicago
 
 --Q10 https://learn.upgrad.com/v/course/77/session/8501/segment/41100
 
-select * from country where NOT(SELECT * FROM COUNTRY is NOT null);
+SET @whereClause = (select CONCAT(REPLACE(group_CONCAT(COLUMN_NAME),',',' IS NULL OR '),' IS NULL') 
+from information_schema.columns where table_schema='world' and table_name = 'country');
+SET @query = CONCAT("SELECT * FROM MOVIE WHERE ",@whereClause); 
+PREPARE stmt FROM @query;
+EXECUTE stmt;
 
-
-select COUNT(*) from country where (
-SELECT concat_ws(select g FROM SELECT group_CONCAT(column_name) AS G FROM (select COLUMN_NAME from information_schema.columns where table_schema='world' and table_name = 'country')A)
-FROM 
-country
-SELECT group_CONCAT(column_name) AS G FROM (select COLUMN_NAME from information_schema.columns where table_schema='world' and table_name = 'country')A
-) IS NULL;
-
-select * from information_schema.columns where table_schema='world' and table_name = 'country' AND table_rows is null
-
-SELECT (
-  select COLUMN_NAME from information_schema.columns where table_schema='world' and table_name = 'country'
-) FROM COUNTRY
-
-
-;with xmlnamespaces('http://www.w3.org/2001/XMLSchema-instance' as ns)
-select 
-       (
-          select T1.*
-          for xml path('row'), elements xsinil, type 
-       ).value('count(//*/@ns:nil)', 'int') as NullCount
-from @T as T1
